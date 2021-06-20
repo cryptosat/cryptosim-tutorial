3453b2ea815634e2c799272d34e09ac98044033b
Gil Shotan <gilsho@cs.stanford.edu>
2021-06-20 19:28:17 +0300

    add sliding menu scaffolding

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,15 +7,38 @@ class NavBar extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      menuVisible: false,
+    }
+    this.onClick = this.onClick.bind(this);
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  onClick(e) {
+    this.toggleMenu();
+    e.stopPropagation();
+  }
+
+  toggleMenu() {
+    this.setState({
+      menuVisible: !this.state.menuVisible
+    });
   }
 
  render() {
+    const visibilityClass = this.state.menuVisible ? 'show' : 'hide';
+    const menu = (
+      <div id='sideMenu' className={visibilityClass}>
+        Hello, World!
+      </div>
+    )
     return(
       <div className='navbar'>
+        {menu}
         Cryptosim Tutorial
-        <a>
-          <FontAwesomeIcon icon={faBars} style={{'float': 'right'}}/>
-        </a>
+        <button onClick={this.onClick}>
+          <FontAwesomeIcon icon={faBars}/>
+        </button>
       </div>
     )
   }
