d90bc390b4dc2460453502edb8e140a92163c4ca
Gil Shotan <gilsho@cs.stanford.edu>
2021-08-18 06:08:19 -0700

    decomposed dragging function

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import './App.css';
 import NavBar from './components/NavBar';
+import ResizablePanelsContainer from './components/ResizablePanelsContainer';
 import plan from './components/lessons/plan';
 import MultipleSatelliteDemo from './components/lessons/MultipleSatelliteDemo';
 import LessonOverview from './components/lessons/LessonOverview';
@@ -38,22 +39,6 @@ const componentMap = new Map([
 ]);
 
 class App extends React.Component {
-
-  constructor() {
-    super();
-    this.contentRef = React.createRef();
-    this.onMouseMove = this.onMouseMove.bind(this);
-    this.onMouseDown = this.onMouseDown.bind(this);
-    this.onMouseUp = this.onMouseUp.bind(this);
-    this.numPanels = 3;
-    this.state = {
-      dragging: false,
-      contentWidth: null,
-      panelWidths: [200, 200, 300],
-      selectedDividerIndex: null,
-      selectedDividerX: null,
-    }
-  }
 
   createRoutes() {
     const flatLessons = [];
@@ -88,111 +73,13 @@ class App extends React.Component {
     return routes;
   }
 
-  componentDidMount() {
-    const dividerWidthPx = 4;
-    const contentWidth = this.contentRef.current.offsetWidth - (
-      (this.numPanels - 1) * dividerWidthPx);
-    this.setState({
-      contentWidth: contentWidth,
-      panelWidths: [200, 400, contentWidth - (200 + 400)],
-    });
-  }
-
-  onMouseDown(index, e) {
-    this.setState({
-      dragging: true,
-      selectedDividerIndex: index,
-      selectedDividerX: e.clientX,
-    })
-  }
-
-  onMouseUp(e) {
-    this.setState({
-      dragging: false,
-    });
-  }
-
-  onMouseMove(e) {
-    if (!this.state.dragging) return;
-    const widths = this.state.panelWidths;
-    const index = this.state.selectedDividerIndex;
-    const expandRight = e.clientX > this.state.selectedDividerX;
-    let maxWidth = this.state.contentWidth;
-    const sum = (arr) => arr.reduce((a, b) => a + b);
-    let delta = Math.abs(e.clientX - this.state.selectedDividerX);
-
-    if (expandRight) {
-      delta = Math.min(delta, sum(widths.slice(index + 1)));
-      widths[index] += delta;
-      let shrinkAmount = -delta;
-      for (let i = index + 1; i < this.numPanels; ++i) {
-        widths[i] += shrinkAmount;
-        if (widths[i] > 0) {
-          break;
-        } else {
-          shrinkAmount = widths[i];
-          widths[i] = 0;
-        }
-      }
-    } else {
-      delta = Math.min(delta, sum(widths.slice(0, index + 1)));
-      widths[index + 1] += delta;
-      let shrinkAmount = -delta;
-      for (let i = index; i >= 0; --i) {
-        widths[i] += shrinkAmount;
-        if (widths[i] > 0) {
-          break;
-        } else {
-          shrinkAmount = widths[i];
-          widths[i] = 0;
-        }
-      }
-    }
-
-    const minWidth = 1;
-    if (widths[0] < minWidth) {
-      let diff = widths[0] - minWidth;
-      widths[0] = minWidth;
-      for (let i = 1; i < this.numPanels; ++i) {
-        if (widths[i] > 0) {
-          widths[i] += diff;
-          if (widths[i] > 0) {
-            break;
-          } else {
-            diff = widths[i]
-            widths[i] = 0;
-          }
-        }
-      }
-    }
-
-    this.setState({
-      selectedDividerX: e.clientX,
-      panelWidths: widths,
-    });
-  }
-
   render() {
     return (
-      <Router>
-        <div className='main'>
-          <div className='content' ref={this.contentRef}
-              onMouseMove={this.onMouseMove}
-              onMouseUp={this.onMouseUp}
-              onMouseLeave={this.onMouseLeave}>
-            <div className='left'
-                 style={{width: this.state.panelWidths[0]}}></div>
-            <div className='divider'
-                 onMouseDown={(e) => {this.onMouseDown(0, e)}} />
-            <div className='middle'
-                 style={{width: this.state.panelWidths[1]}}></div>
-            <div className='divider'
-                 onMouseDown={(e) => {this.onMouseDown(1, e)}} />
-            <div className='right'
-                 style={{width: this.state.panelWidths[2]}}></div>
-          </div>
-        </div>
-      </Router>
+      <ResizablePanelsContainer>
+        <div className='left' />
+        <div className='middle' />
+        <div className='right' />
+      </ResizablePanelsContainer>
     );
   }
 
