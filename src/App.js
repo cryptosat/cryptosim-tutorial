a2a3da9a13394b08142ba29a0fd752a30147ac18
C <45455218+crypblizz8@users.noreply.github.com>
2022-07-12 12:01:04 +0200

    fix width of the map

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   HashRouter as Router,
   Switch,
@@ -6,51 +6,49 @@ import {
   Redirect,
   withRouter,
 } from "react-router-dom";
-import tweetnacl from 'tweetnacl';
-import './App.css';
-import NavBar from './components/NavBar';
-import PanelContainer from './components/PanelContainer';
-import plan from './components/lessons/plan';
-import MultiSatDemo from './components/MultiSatDemo';
-import Lesson from './components/Lesson';
+import tweetnacl from "tweetnacl";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import PanelContainer from "./components/PanelContainer";
+import plan from "./components/lessons/plan";
+import MultiSatDemo from "./components/MultiSatDemo";
+import Lesson from "./components/Lesson";
 
-import overview from './components/lessons/overview';
-import communication from './components/lessons/communication';
-import asynchrony from './components/lessons/asynchrony';
-import version from './components/lessons/version';
-import publicKeys from './components/lessons/publicKeys';
-import timestamp from './components/lessons/timestamp';
-import publicRandomness from './components/lessons/publicRandomness';
-import privateRandomness from './components/lessons/privateRandomness';
-import signature from './components/lessons/signature';
-import nextOnline from './components/lessons/nextOnline';
+import overview from "./components/lessons/overview";
+import communication from "./components/lessons/communication";
+import asynchrony from "./components/lessons/asynchrony";
+import version from "./components/lessons/version";
+import publicKeys from "./components/lessons/publicKeys";
+import timestamp from "./components/lessons/timestamp";
+import publicRandomness from "./components/lessons/publicRandomness";
+import privateRandomness from "./components/lessons/privateRandomness";
+import signature from "./components/lessons/signature";
+import nextOnline from "./components/lessons/nextOnline";
 
-import { Map as WorldMap } from '@cryptosat/cryptosim-visualization';
-import { Console } from '@cryptosat/jsconsole';
+import { Map as WorldMap } from "@cryptosat/cryptosim-visualization";
+import { Console } from "@cryptosat/jsconsole";
 
-import SimulatedClock from '@cryptosat/cryptosim/lib/clocks/simulatedClock';
-import GeoCoordinates from '@cryptosat/cryptosim/lib/geoCoordinates';
-import GroundStationNetwork from '@cryptosat/cryptosim/lib/groundStationNetwork';
-import Universe from '@cryptosat/cryptosim/lib/universe';
-import Satellite from '@cryptosat/cryptosim/lib/satellite';
-import MainService from '@cryptosat/cryptosim/lib/services/main';
-import MainClient from '@cryptosat/cryptosim/lib/clients/main';
-import binary from '@cryptosat/cryptosim/lib/binary';
-
+import SimulatedClock from "@cryptosat/cryptosim/lib/clocks/simulatedClock";
+import GeoCoordinates from "@cryptosat/cryptosim/lib/geoCoordinates";
+import GroundStationNetwork from "@cryptosat/cryptosim/lib/groundStationNetwork";
+import Universe from "@cryptosat/cryptosim/lib/universe";
+import Satellite from "@cryptosat/cryptosim/lib/satellite";
+import MainService from "@cryptosat/cryptosim/lib/services/main";
+import MainClient from "@cryptosat/cryptosim/lib/clients/main";
+import binary from "@cryptosat/cryptosim/lib/binary";
 
 const componentMap = new Map([
-  ['overview', overview],
-  ['communication', communication],
-  ['asynchrony', asynchrony],
-  ['version', version],
-  ['publicKeys', publicKeys],
-  ['timestamp', timestamp],
-  ['publicRandomness', publicRandomness],
-  ['privateRandomness', privateRandomness],
-  ['signature', signature],
-  ['nextOnline', nextOnline],
-])
-
+  ["overview", overview],
+  ["communication", communication],
+  ["asynchrony", asynchrony],
+  ["version", version],
+  ["publicKeys", publicKeys],
+  ["timestamp", timestamp],
+  ["publicRandomness", publicRandomness],
+  ["privateRandomness", privateRandomness],
+  ["signature", signature],
+  ["nextOnline", nextOnline],
+]);
 
 /* This is an ugly hack in order to make MultiSatDemo override the entire
  * page. */
@@ -66,7 +64,6 @@ class AppContainer extends React.Component {
 }
 
 class App extends React.Component {
-
   constructor() {
     super();
     this.setupUniverse();
@@ -90,16 +87,18 @@ class App extends React.Component {
     const universe = new Universe(clock);
 
     const ISS_TLE = [
-      '1 25544U 98067A   21027.77992426  .00003336  00000-0  68893-4 0  9991',
-      '2 25544  51.6465 317.1909 0002399 302.6503 164.1536 15.48908950266831',
+      "1 25544U 98067A   21027.77992426  .00003336  00000-0  68893-4 0  9991",
+      "2 25544  51.6465 317.1909 0002399 302.6503 164.1536 15.48908950266831",
     ];
 
-    const sat = new Satellite(universe, 'crypto1', ISS_TLE[0], ISS_TLE[1]);
-    const gsnetwork = GroundStationNetwork.load(universe,
-      require('@cryptosat/cryptosim/data/rbcNetwork'));
+    const sat = new Satellite(universe, "crypto1", ISS_TLE[0], ISS_TLE[1]);
+    const gsnetwork = GroundStationNetwork.load(
+      universe,
+      require("@cryptosat/cryptosim/data/rbcNetwork")
+    );
     const mainService = new MainService(universe);
-    sat.bindService('main', mainService);
-    const client = new MainClient(universe, sat, gsnetwork, 'main');
+    sat.bindService("main", mainService);
+    const client = new MainClient(universe, sat, gsnetwork, "main");
 
     this.payload = {
       cryptosat: client,
@@ -123,7 +122,7 @@ class App extends React.Component {
       const lesson = flatLessons[i];
       let next = null;
       let previous = null;
-      if  (i > 0) {
+      if (i > 0) {
         const previousLesson = flatLessons[i - 1];
         previous = previousLesson.disabled ? null : previousLesson.path;
       }
@@ -143,7 +142,7 @@ class App extends React.Component {
   }
 
   render() {
-    if (this.props.location.pathname === '/multisat') {
+    if (this.props.location.pathname === "/multisat") {
       return <MultiSatDemo />;
     }
 
@@ -152,46 +151,50 @@ class App extends React.Component {
     const routes = this.createRoutes();
 
     const leftPanelContent = (
-            <Switch>
-              {routes}
-              <Route path="/">
-                <Redirect to='/getting-started/overview' />
-              </Route>
-            </Switch>
-    )
+      <Switch>
+        {routes}
+        <Route path="/">
+          <Redirect to="/getting-started/overview" />
+        </Route>
+      </Switch>
+    );
 
     const middlePanelContent = (
-        <div className='map-hole'>
-          <WorldMap universe={this.universe}
-                 gsnetwork={this.gsnetwork}
-                 center={center} zoom={zoom}/>
-        </div>
-    )
+      <div className="map-hole">
+        <WorldMap
+          universe={this.universe}
+          gsnetwork={this.gsnetwork}
+          center={center}
+          zoom={zoom}
+        />
+      </div>
+    );
 
     return (
-      <div className='main'>
-        <div className='nav-container'>
+      <div className="main">
+        <div className="nav-container">
           <NavBar />
         </div>
-        <div className='content'>
+        <div className="content">
           {leftPanelContent}
-          {/* <PanelContainer> */}
-            {/* <div style={{ display: 'flex', flexDirection: "column", height: "100%", border:"1px solid red" }}> */}
-              {/* {middlePanelContent} */}
-              {/* <Console theme='dark' payload={this.payload}/> */}
-            {/* </div> */}
-          {/* </PanelContainer> */}
-          <div style={{ display: 'flex', flexDirection: "column", height: "100%", paddingTop: '2em', paddingBottom: '2em', marginRight: '2em' }}>
-            {/* <Console theme='dark' payload={this.payload} /> */}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              height: "100%",
+              paddingTop: "2em",
+              paddingBottom: "2em",
+              marginRight: "2em",
+              width: "100%",
+            }}
+          >
             {middlePanelContent}
-            <Console theme='dark' payload={this.payload} />
+            <Console theme="dark" payload={this.payload} />
           </div>
-
         </div>
       </div>
     );
   }
-
 }
 
 export default AppContainer;
