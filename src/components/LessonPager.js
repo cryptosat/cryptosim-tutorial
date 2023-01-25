8c43e4ec5c21aaba615e15ccefd72c557a1fff20
JosephOrigin <josepha@reallygreattech.com>
2023-01-25 12:58:41 +0000

    Progress bar and Pagination

--- src/components/LessonPager.js
+++ src/components/LessonPager.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import './LessonPager.css';
 
 
@@ -10,12 +10,14 @@ class LessonPager extends React.Component {
   render() {
     let previous = null;
     let next = null;
-    
+    let totalPages = this.props.totalPages ;
+    let currentPage = this.props.currentPage ;
+
     if (this.props.previous) {
       previous = (
         <Link to={this.props.previous}>
           <span className='arrow'>
-            <FontAwesomeIcon icon={faArrowLeft}/>
+            <FontAwesomeIcon icon={faChevronLeft} />
           </span>
           <span className='caption'>Previous</span>
         </Link>
@@ -25,19 +27,25 @@ class LessonPager extends React.Component {
     if (this.props.next) {
       next = (
         <Link to={this.props.next}>
-          <span className='caption'>Next</span>
           <span className='arrow'>
-            <FontAwesomeIcon icon={faArrowRight}/>
+            <FontAwesomeIcon icon={faChevronRight} />
           </span>
+          <span className='caption'>Next</span>
         </Link>
       );
     }
 
     return (
-      <div className="pager">
-        <div className={'link-container previous'}>{previous}</div>
-        <div className='link-container next'>{next}</div>
-      </div>
+      <>
+        <div className="pager">
+          <div className={'link-container previous'}>{previous}</div>
+          <div className={'link-container numbering'}>{currentPage}/{totalPages}</div>
+          <div className='link-container next'>{next}</div>
+        </div>
+        <div className='progress-bar'>
+          <div className='progress-fill' style={{width:`${(currentPage/totalPages)*100}%`}}></div>
+        </div>
+      </>
     );
   }
 
