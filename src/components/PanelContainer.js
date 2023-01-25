d4d05babaa7ca97465ac97cf41126bef9ae1f6d2
wevs <68214494+dopewevmond@users.noreply.github.com>
2023-01-25 12:01:57 +0000

    Allowing for the toggling of the instruction panel container to expand and collapse

--- src/components/PanelContainer.js
+++ src/components/PanelContainer.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import './PanelContainer.css';
+import { faAnglesLeft } from '@fortawesome/pro-thin-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class PanelContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setCollapsed = this.setCollapsed.bind(this);
+    this.toggleCollapsed = this.toggleCollapsed.bind(this);
+    this.state = {
+      isCollapsed: false
+    };
+  }
+
+  setCollapsed(instructionsCollapsed) {
+    this.setState({ instructionsCollapsed });
+  }
+
+  toggleCollapsed(e) {
+    e.stopPropagation();
+    this.setCollapsed(!this.state.instructionsCollapsed);
+  }
 
   render() {
+    const panelClassName = this.state.instructionsCollapsed
+      ? 'panel-container__left instructions-collapsed'
+      : 'panel-container__left';
+    const instructionsPanelContent = !this.state.instructionsCollapsed ? this.props.children[2]: null;
+
     return (
       <div className='panel-container'>
-        <div className='panel-container__left'>
-          <div className='instructionsPanel'>{this.props.children[2]}</div>
+        <div className={panelClassName}>
+          <nav>
+            <button onClick={this.toggleCollapsed}>
+              <FontAwesomeIcon icon={faAnglesLeft} />
+            </button>
+          </nav>
+          <div className='instructionsPanel'>{instructionsPanelContent}</div>
         </div>
         <div className='panel-container__right'>
           <div className='mapPanel'>{this.props.children[1]}</div>
