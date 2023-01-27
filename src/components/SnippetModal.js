c53efce9e050aa0a8d3f836831052225f00af115
JosephOrigin <josepha@reallygreattech.com>
2023-01-27 14:59:57 +0000

    Update to code snippet font

--- src/components/SnippetModal.js
+++ src/components/SnippetModal.js
@@ -1,29 +1,29 @@
 import React from 'react'
 import './SnippetModal.css'
-import { faCopy} from '@fortawesome/pro-light-svg-icons'
+import { faCopy } from '@fortawesome/pro-light-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/pro-thin-svg-icons'
 
-const SnippetModal = ({copy,code,setShow}) => {
-  return (
-      <>
-          <div id='modal-main' onClick={() => setShow(false)}>
-          </div>
-          <div className='container' >
-                  <div className='exit-icon' onClick={() => setShow(false)} >
-                      <FontAwesomeIcon icon={faXmark} size='lg' />
-                  </div>
-                  <div className='modal-content'>
-                      <div className='modal-content-code'>
-                          <p>{code}</p>
-                      </div>
-                      <div className='copy-icon' onClick={() => copy()}>
-                        <FontAwesomeIcon icon={faCopy} size='lg' />
-                      </div>
-                  </div>
+const SnippetModal = ({ copy, code, setShow }) => {
+    return (
+        <>
+            <div id='modal-main' onClick={() => setShow(false)}>
             </div>
-      </>
-  )
+            <div className='container' >
+                <div className='exit-icon' onClick={() => setShow(false)} >
+                    <FontAwesomeIcon icon={faXmark} size='lg' />
+                </div>
+                <div className='modal-content'>
+                    <div className='modal-content-code'>
+                        <pre>{code}</pre>
+                    </div>
+                    <div className='copy-icon' onClick={() => copy()}>
+                        <FontAwesomeIcon icon={faCopy} size='lg' />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default SnippetModal;
