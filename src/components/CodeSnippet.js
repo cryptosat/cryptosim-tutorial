ed38ad9618862d5e71d54be1ee6f71e687dfa65d
razak <razakwasiu@gmail.com>
2023-02-03 07:17:57 +0000

    add snippet functionality

--- src/components/CodeSnippet.js
+++ src/components/CodeSnippet.js
@@ -1,67 +1,36 @@
 import React, { useState } from "react";
 import "./CodeSnippet.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExpandAlt, faCopy } from "@fortawesome/pro-light-svg-icons";
+import { faExpandAlt, faCopy, faPaste } from "@fortawesome/pro-light-svg-icons";
 import SnippetModal from "./SnippetModal";
 
-// class CodeSnippet extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       showModal: false,
-//     };
-//     this.setShowModal = this.setShowModal.bind(this);
-//     this.copy = this.copy.bind(this);
-//   }
-
-//   setShowModal(modalState) {
-//     this.setState({
-//       showModal: modalState,
-//     });
-//   }
-
-//   copy() {
-//     copyToClipboard(this.props.code);
-//   }
-
-//   render() {
-//     return (
-//       <div className="code-snippet">
-//         <pre>
-//           <code>{this.props.code}</code>
-//           <div className="icon-container">
-//             <button onClick={() => this.setShowModal(!this.state.showModal)}>
-//               <FontAwesomeIcon icon={faExpandAlt} />
-//             </button>
-//             <button onClick={this.copy.bind(this)}>
-//               <FontAwesomeIcon icon={faCopy} /> Copy
-//             </button>
-//           </div>
-//         </pre>
-//         {this.state.showModal && (
-//           <SnippetModal
-//             copy={this.copy}
-//             code={this.props.code}
-//             setShow={this.setShowModal}
-//           />
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
+const textarea = document.querySelector(".cli");
 function copyToConsole(text) {
-  const textarea = document.querySelector(".cli");
   if (textarea) {
     textarea.value = text;
     textarea.focus();
-    textarea.style.height = 'auto';
-    textarea.style.height = textarea.scrollHeight + 'px';
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
   }
 }
 
 class CodeSnippet extends React.Component {
-  copy() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    };
+    this.setShowModal = this.setShowModal.bind(this);
+    this.pasteToConsole = this.pasteToConsole.bind(this);
+  }
+
+  setShowModal(modalState) {
+    this.setState({
+      showModal: modalState,
+    });
+  }
+
+  pasteToConsole() {
     copyToConsole(this.props.code);
   }
 
@@ -71,11 +40,21 @@ class CodeSnippet extends React.Component {
         <pre>
           <code>{this.props.code}</code>
           <div className="icon-container">
-            <button onClick={this.copy.bind(this)}>
-              Copy <FontAwesomeIcon icon={faCopy} />
+            <button onClick={() => this.setShowModal(!this.state.showModal)}>
+              <FontAwesomeIcon color="#ffffff" icon={faExpandAlt} />
+            </button>
+            <button onClick={this.pasteToConsole.bind(this)}>
+              <FontAwesomeIcon color="#ffffff" icon={faPaste} />
             </button>
           </div>
         </pre>
+        {this.state.showModal && (
+          <SnippetModal
+            pasteToConsole={this.pasteToConsole}
+            code={this.props.code}
+            setShow={this.setShowModal}
+          />
+        )}
       </div>
     );
   }
